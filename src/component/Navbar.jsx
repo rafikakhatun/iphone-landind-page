c1982aa426c21e587c023672927773fb9fca0ed7
rafika khatun <rafikakhatun607@gmail.com>
2025-07-04 12:33:45 +0530

    Initial commit: React + Tailwind CSS project setup

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,25 @@
+function Navbar() {
+    return (
+
+
+        <>
+
+            <section className="w-11/12 mx-auto">
+                <nav className="p-8 flex justify-between">
+                    <h1 className="text-4xl font-bold bg-gradient-to-r from-pink-500 to-blue-500 text-transparent bg-clip-text">iPhoneWorld</h1>
+                    <button className="px-8 py-2 bg-blue-600 rounded-md text-white text-lg">Login</button>
+
+                </nav>
+            </section>
+
+
+        </>
+
+
+
+
+
+    )
+}
+
+export default Navbar;
